b284df58416573d6c3324e4a1b311cb2dc0bbaac
TopherAllenCook <TopherAllenCook@users.noreply.github.com>
2026-05-13 21:01:57 -0600

    feat: wire up Verve MD homepage with all sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,33 @@
-import NavV2 from '@/components/v2/NavV2'
-import HeroV2 from '@/components/v2/HeroV2'
-import ManifestoV2 from '@/components/v2/ManifestoV2'
-import SearchMockupV2 from '@/components/v2/SearchMockupV2'
-import ServicesV2 from '@/components/v2/ServicesV2'
-import ProcessV2 from '@/components/v2/ProcessV2'
-import ProofV2 from '@/components/v2/ProofV2'
-import CtaV2 from '@/components/v2/CtaV2'
-import FooterV2 from '@/components/v2/FooterV2'
+import NavVerve from '@/components/verve/NavVerve'
+import HeroVerve from '@/components/verve/HeroVerve'
+import LogoStripVerve from '@/components/verve/LogoStripVerve'
+import PainPointsVerve from '@/components/verve/PainPointsVerve'
+import ResultsVerve from '@/components/verve/ResultsVerve'
+import PackagesOverviewVerve from '@/components/verve/PackagesOverviewVerve'
+import AIToolsVerve from '@/components/verve/AIToolsVerve'
+import AEOBlockVerve from '@/components/verve/AEOBlockVerve'
+import TestimonialsVerve from '@/components/verve/TestimonialsVerve'
+import WhoWeWorkWithVerve from '@/components/verve/WhoWeWorkWithVerve'
+import BottomCTAVerve from '@/components/verve/BottomCTAVerve'
+import FooterVerve from '@/components/verve/FooterVerve'
 
 export default function HomePage() {
   return (
     <>
-      <NavV2 />
+      <NavVerve />
       <main>
-        <HeroV2 />
-        <ManifestoV2 />
-        <SearchMockupV2 />
-        <ServicesV2 />
-        <ProcessV2 />
-        <ProofV2 />
-        <CtaV2 />
+        <HeroVerve />
+        <LogoStripVerve />
+        <PainPointsVerve />
+        <ResultsVerve />
+        <PackagesOverviewVerve />
+        <AIToolsVerve />
+        <AEOBlockVerve />
+        <TestimonialsVerve />
+        <WhoWeWorkWithVerve />
+        <BottomCTAVerve />
       </main>
-      <FooterV2 />
+      <FooterVerve />
     </>
   )
 }
